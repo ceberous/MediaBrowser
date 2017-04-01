b5eae4857a58d3e5293fde4a102e82cd044cdbdc
morpheous <cerbus.collin@gmail.com>
2017-04-01 01:28:17 -0400

    16

--- server/py_scripts/buttonWatcher.py
+++ server/py_scripts/buttonWatcher.py
@@ -10,35 +10,41 @@ for event in gamepad.read_loop():
         if keyevent.keystate == KeyEvent.key_up:
             
             if keyevent.keycode[0] == 'BTN_JOYSTICK':
-                print "1"
-                sys.stdout.flush()
-            elif keyevent.keycode == 'BTN_THUMB':
-                print "2"
-                sys.stdout.flush()
-            elif keyevent.keycode == 'BTN_THUMB2':
-                print "3"
-                sys.stdout.flush()
-            elif keyevent.keycode == 'BTN_TOP':
-                print "4"
-                sys.stdout.flush()
-            elif keyevent.keycode == 'BTN_TOP2':
-                print "5"
-                sys.stdout.flush()
-            elif keyevent.keycode == 'BTN_PINKIE':
                 print "6"
                 sys.stdout.flush()
-                
-            '''
-            elif keyevent.keycode == 'BTN_???':
+            elif keyevent.keycode == 'BTN_THUMB':
                 print "7"
-            elif keyevent.keycode == 'BTN_???':
-                print "8"
-            elif keyevent.keycode == 'BTN_???':
-                print "9"
-            elif keyevent.keycode == 'BTN_???':
+                sys.stdout.flush()
+            elif keyevent.keycode == 'BTN_THUMB2':
                 print "10"
-            elif keyevent.keycode == 'BTN_???':
+                sys.stdout.flush()
+            elif keyevent.keycode == 'BTN_TOP':
                 print "11"
-            elif keyevent.keycode == 'BTN_???':
+                sys.stdout.flush()
+            elif keyevent.keycode == 'BTN_TOP2':
                 print "12"
-            '''
+                sys.stdout.flush()
+            elif keyevent.keycode == 'BTN_PINKIE':
+                print "8"
+                sys.stdout.flush()
+            elif keyevent.keycode == 'BTN_BASE':
+                print "9"
+                sys.stdout.flush()                
+            elif keyevent.keycode == 'BTN_BASE4': 
+                print "1"
+                sys.stdout.flush() 
+            elif keyevent.keycode == 'BTN_BASE5':
+                print "2"
+                sys.stdout.flush() 
+            elif keyevent.keycode == 'BTN_BASE6':
+                print "3"
+                sys.stdout.flush() 
+            elif keyevent.keycode == 'BTN_BASE2':
+                print "4"
+                sys.stdout.flush() 
+            elif keyevent.keycode == 'BTN_BASE3':
+                print "5"
+                sys.stdout.flush() 
+            elif keyevent.keycode == 'BTN_BASE4':
+                print "12"
+                sys.stdout.flush() 
