e6c5156746d5e74d90b6112a0212688c06b4affb
ceberous <cerbus.collin@gmail.com>
2017-04-25 16:44:36 -0400

    44

--- server/localVideoManager.js
+++ server/localVideoManager.js
@@ -4,12 +4,15 @@ var wEmitter = new (require('events').EventEmitter);
 
 var fs = require('fs');
 var path = require("path");
+var spawn = require("child_process").spawn;
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
 require('shelljs/global');
 var dirTree = require("directory-tree");
 var jsonfile = require("jsonfile");
 var Filehound = require("filehound");
 var mime = require('mime');
-var MPlayer = require("mplayer");
+
 
 
 var mediaFiles = {
@@ -105,10 +108,10 @@ var mediaFiles = {
 
 		}
 
-		var finalResults = {};
+		var finalResults = {globalLastWatched: {}};
 		var wIndex = 0;
 		for ( var iprop in wList ) {
-			finalResults[iprop] = {lastWached: {}};
+			finalResults[iprop] = { lastWached: {} , totalItems: wList[iprop].length };
 			for ( var j = 0; j < wList[iprop].length; ++j ) {
 
 				wList[iprop][j] = wList[iprop][j].filter(String);
@@ -140,81 +143,6 @@ var mediaFiles = {
 
 };
 
-var wMPWrapper = {
-
-	wPlayer: null,
-
-	state: {
-		muted: false,
-		playing: false,
-		volume: 0,
-		duration: 0,
-		fullscreen: false,
-		subtitles: false,
-		resumeTime: 0,
-	},
-
-	init: function() {
-		wMPWrapper.wPlayer = new MPlayer();
-	},
-
-	watch: function(wFP) {
-		if ( wMPWrapper.wPlayer === null ) { wMPWrapper.init(); }
-		wMPWrapper.wPlayer.openFile( wFP );
-		wMPWrapper.wPlayer.on( 'ready' , (wStatus)=> { wMPWrapper.onReady(wStatus) });
-	},
-
-	onReady: function(wStatus) {
-		//wMPWrapper.wPlayer.mute();
-		wMPWrapper.wPlayer.volume(100);
-		setTimeout( ()=> { wMPWrapper.wPlayer.hideSubtitles();  } , 2000 );
-		wMPWrapper.wPlayer.on( 'status' , (wStatus)=> { wMPWrapper.onStatus(wStatus) });
-		wMPWrapper.wPlayer.on( 'play' , (wEvent)=> { wMPWrapper.onPlay(wEvent); });
-		wMPWrapper.wPlayer.on( 'pause' , (wEvent)=> { wMPWrapper.onPause(wEvent); });
-		wMPWrapper.wPlayer.on( 'stop' , (wEvent)=> { wMPWrapper.onStop(wEvent); });
-		wMPWrapper.wPlayer.on( 'time' , (wTime)=> { wMPWrapper.onTime(wTime); });
-	},
-
-	onStatus: function(wStatus) {
-		if ( wStatus.playing && wStatus.filename !== null ) {
-			console.log(wStatus);
-			wMPWrapper.state.muted 		= wStatus.muted;
-			wMPWrapper.state.playing 	= wStatus.playing;
-			wMPWrapper.state.volume 	= wStatus.volume;
-			wMPWrapper.state.duration 	= wStatus.duration;
-			wMPWrapper.state.fullscreen = wStatus.fullscreen;
-			wMPWrapper.state.subtitles 	= wStatus.subtitles;
-		}
-	},
-
-	onPlay: function(wEvent) {
-
-	},
-
-	onPause: function(wEvent) {
-
-	},
-
-	onStop: function(wEvent) {
-		//console.log( "playback has ended" );
-		return true;
-	},
-
-	onTime: function(wTime) {
-		wMPWrapper.state.resumeTime = wTime;
-	},
-
-	pause: function() {
-		wMPWrapper.wPlayer.pause();
-	},
-
-	stop: function() {
-		wMPWrapper.wPlayer.stop();
-		wMPWrapper.wPlayer = null;
-	},
-
-};
-
 
 var wPM = {
 
@@ -223,6 +151,9 @@ var wPM = {
 	genreIndex: null,
 	genreFolders: [],
 	lastWachedinGenre: null,
+	globalLastWatched: null,
+
+	wPlayer: null,
 
 	nowPlaying: {},
 
@@ -232,51 +163,43 @@ var wPM = {
 		next: [],
 	},
 
-	prepare: function( wRandom , wGenre ) {
+	state: {
+		paused: false,
+	},
 
-		wPM.randomMode = wRandom;
+	playRandom: function( wGenre ) {
+
+		wPM.randomMode = true;
 		wPM.genre = wGenre;
 		wPM.genreIndex = mediaFiles.watchedList.rootMap[wGenre];
 		wPM.lastWachedinGenre = mediaFiles.watchedList[wGenre].lastWached;
-		
-		for ( var iprop in mediaFiles.watchedList[wGenre] ) {
-			wPM.genreFolders.push(iprop);
-			wPM.genreFolders.push( mediaFiles.watchedList[wGenre][iprop].index );
-		}
-		
-		if (wRandom) {
-			var wEpisode = wPM.getRandom();
-			wPM.nowPlaying.name = wEpisode.name;
-			wPM.nowPlaying.path = wEpisode.path;
-			wPM.nowPlaying.tableMap = wEpisode.tableMap;
-			console.log(wPM.nowPlaying);
-			wMPWrapper.watch( wPM.nowPlaying.path );
 
-		}
-		else {
-			var wEpisode = wPM.getNext();
-		}
+		wPM.nowPlaying = wPM.getRandom();
+		wPM.startPlayer();
+		wEmitter.emit("mPlayerPlaying");
 
 	},
 
 	getRandom: function() {
 
-		var randomShowIndex = wPM.getRandomVal( 0 , wPM.genreFolders[ wPM.genreFolders.length - 1 ] );
+		var genreFoldersLength = mediaFiles.watchedList[wPM.genre].totalItems - 1;
+		var randomShowIndex = wPM.getRandomVal( 0 , genreFoldersLength );
 		var randomShowName = mediaFiles.structure.children[ wPM.genreIndex ].children[ randomShowIndex ].name;
 		var wFolderMax = mediaFiles.watchedList[wPM.genre][randomShowName].items.length - 1;
 		var randomShowFolderIndex = wPM.getRandomVal( 0 , wFolderMax );
 		var wEpisodeMax = mediaFiles.watchedList[wPM.genre][randomShowName].items[randomShowFolderIndex] - 1;
 		var randomEpisodeIndex = wPM.getRandomVal( 0 , wEpisodeMax );
 
-		console.log( "Folder Max = " + wFolderMax.toString() );
-		console.log( "Episode Max = " + wEpisodeMax.toString() );
-		console.log( "Genre Index = " + wPM.genreIndex.toString() );
-		console.log( "Show Index = " + randomShowIndex.toString() );
-		console.log( "ShowSubFolder Index = " + randomShowFolderIndex.toString() );
-		console.log( "ShowItem Index = " + randomEpisodeIndex.toString() );
+		//console.log( "Genre Index = " + wPM.genreIndex.toString() );
+		//console.log( "Show Index = " + randomShowIndex.toString() );
+		//console.log( "Folder Max = " + wFolderMax.toString() );
+		//console.log( "ShowSubFolder Index = " + randomShowFolderIndex.toString() );
+		//console.log( "Episode Max = " + wEpisodeMax.toString() );
+		//console.log( "ShowItem Index = " + randomEpisodeIndex.toString() );
 		
 		var wEpisode , newTableMapEntry;
 
+		// More Rediculousness 
 		switch( wPM.genre ) {
 			case "TV Shows":
 				wEpisode = wPM.tableLookUp( wPM.genreIndex , randomShowIndex , randomShowFolderIndex , randomEpisodeIndex  );
@@ -311,7 +234,47 @@ var wPM = {
 
 	},
 
-	getNext: function() {
+	playNextInTVShow: function( wShowName ) {
+
+		var wGenreIndex = mediaFiles.watchedList.rootMap["TV Shows"];
+		var wShowIndex = mediaFiles.watchedList["TV Shows"][wShowName].index;
+		var wFolderMax = mediaFiles.watchedList["TV Shows"][wShowName].items.length - 1;
+		var wEpisodeMax;
+		var wFolderIndex = 0; 
+		var wEpisodeIndex = 0;
+		
+		console.log(mediaFiles.watchedList["TV Shows"][wShowName]);
+		if ( mediaFiles.watchedList["TV Shows"][wShowName]["lastWached"].length > 1 ) {
+			wFolderIndex = mediaFiles.watchedList["TV Shows"][wShowName].lastWached[0];
+			wEpisodeIndex = mediaFiles.watchedList["TV Shows"][wShowName].lastWached[1] + 1;
+			wEpisodeMax = ( mediaFiles.watchedList["TV Shows"][wShowName].items[wFolderIndex] - 1 );
+		}
+		
+		if ( wEpisodeIndex > wEpisodeMax ) { wEpisodeIndex = 0; wFolderIndex += 1; }
+		if ( wFolderIndex > wFolderMax ) { wFolderIndex = 0; }
+		
+		//console.log( "GenreIndex = " + wGenreIndex.toString() );
+		//console.log( "ShowIndex = " + wShowIndex.toString() );
+		//console.log( "FolderIndex = " + wFolderIndex.toString() );
+		//console.log( "EpisodeIndex = " + wEpisodeIndex.toString() );
+
+		var wEpisode = wPM.tableLookUp( wGenreIndex , wShowIndex , wFolderIndex , wEpisodeIndex );
+
+		mediaFiles.watchedList["TV Shows"][wShowName]["lastWached"][0] = wFolderIndex;
+		mediaFiles.watchedList["TV Shows"][wShowName]["lastWached"][1] = wEpisodeIndex;
+
+		wPM.tableMap.previous = wPM.tableMap.now;
+		wPM.tableMap.now = [ "TV Shows" , wShowIndex , wFolderIndex , wEpisodeIndex ];
+		
+		wPM.nowPlaying.name = wEpisode.name;
+		wPM.nowPlaying.path = wEpisode.path;
+		wPM.nowPlaying.tableMap = wPM.tableMap.now;
+		wPM.startPlayer();
+		wEmitter.emit("mPlayerPlaying");
+
+	},
+
+	getNextInCycle: function( wGenre ) {
 
 	},
 
@@ -319,6 +282,7 @@ var wPM = {
 		return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 	},
 
+	// my other autistic moment
 	tableLookUp: function( genreIndex , showIndex , showSubFolderIndex , itemIndex ) {
 
 		if ( !itemIndex ) {
@@ -346,7 +310,125 @@ var wPM = {
 			return mediaFiles.structure.children[ genreIndex ].children[ showIndex ].children[ showSubFolderIndex ].children[ itemIndex ];
 		}
 	
-	}
+	},
+
+	startPlayer: function() {
+
+		console.log(wPM.nowPlaying);
+
+		mediaFiles.watchedList.globalLastWatched = wPM.nowPlaying;
+		jsonfile.writeFileSync( mediaFiles.hardDrive.watchedListFP , mediaFiles.watchedList );
+		
+		var wOptions = {
+		
+		};
+		
+		var defaultArgs = [ wPM.nowPlaying.path , '-msglevel', 'global=0', '-msglevel', 'cplayer=4', '-idle', '-slave', '-fs', '-noborder'];
+		wPM.wPlayer = spawn( "mplayer" , defaultArgs , wOptions );
+		wEmitter.emit("mPlayerPlaying");
+
+		var ignoreMessages = [
+			"MPlayer 1.2.1 (Debian), built with gcc-5.3.1 (C) 2000-2016 MPlayer Team",
+			"Playing /media/haley/Seagate Expansion Drive/TV Shows/Parks and Recreation/5/Parks and Recreation - S05E22 - Are You Better Off.mp4.",
+			"libavformat version 56.40.101 (external)",
+			"libavformat file format detected.",
+			"[mov,mp4,m4a,3gp,3g2,mj2 @ 0x7f89ed539d80]",
+			"Protocol name not provided, cannot determine if input is local or a network protocol, buffers and access patterns cannot be configured optimally without knowing the protocol",
+			"[lavf] stream 0: video (h264), -vid 0",
+			"[lavf] stream 1: audio (aac), -aid 0",
+			", -alang und",
+			"VIDEO:  [H264]  720x404  24bpp  23.976 fps  1277.2 kbps (155.9 kbyte/s)",
+			"Clip info:",
+			 "major_brand: isom",
+			 "minor_version: 1",
+			 "compatible_brands: isom",
+			 "creation_time: 2013-05-02 01:41:44",
+			"Load subtitles in /media/haley/Seagate Expansion Drive/TV Shows/Parks and Recreation/5/",
+			"==========================================================================",
+			"Opening video decoder: [ffmpeg] FFmpeg's libavcodec codec family",
+			"libavcodec version 56.60.100 (external)",
+			"Selected video codec: [ffh264] vfm: ffmpeg (FFmpeg H.264)",
+			"Opening audio decoder: [ffmpeg] FFmpeg/libavcodec audio decoders",
+			"AUDIO: 48000 Hz, 2 ch, floatle, 107.5 kbit/3.50% (ratio: 13436->384000)",
+			"Selected audio codec: [ffaac] afm: ffmpeg (FFmpeg AAC (MPEG-2/MPEG-4 Audio))",
+			"AO: [pulse] 48000Hz 2ch floatle (4 bytes per sample)",
+			"Starting playback...",
+			"Movie-Aspect is undefined - no prescaling applied.",
+			"VO: [xv] 720x404 => 720x404 Planar YV12  [fs]"
+		];
+		var ignoreMessagesLength = ignoreMessages.length;
+
+		var ignoreErrorMessages = [
+			"do_connect: could not connect to socket",
+			"connect: Connection refused",
+			"Failed to open LIRC support. You will not be able to use your remote control.",
+			"Failed to open VDPAU backend libvdpau_va_gl.so: cannot open shared object file: No such file or directory",
+			"[vdpau] Error when calling vdp_device_create_x11: 1",
+			"[ac3 @ 0x7f4459fc3560]Channel layout '5.1(side)' with 6 channels does not match specified number of channels 2: ignoring specified channel",
+			"layou",
+		];
+		var ignoreErrorMessagesLength = ignoreErrorMessages.length;
+
+		var ignore = false;
+		wPM.wPlayer.stdout.on( "data" , function(data) {
+				
+				var message = decoder.write(data);
+				message = message.trim();
+				if ( data.indexOf('A:') != 0 )  {
+					for ( var i = 0; i < ignoreMessagesLength; ++i ) {
+						if ( message === ignoreMessages[i] ) {
+							ignore = true;
+						}
+					}
+					if ( !ignore ) {
+						console.log(message);	
+					} 
+				}
+				
+		});
+
+		wPM.wPlayer.stderr.on( "data" , function(data) {
+				var message = decoder.write(data);
+				message = message.trim();
+				for ( var i = 0; i < ignoreErrorMessagesLength; ++i ) {
+					if ( message === ignoreErrorMessages[i] ) {
+						ignore = true;
+					}
+				}
+
+				if ( !ignore ) {
+					console.log(message);	
+				} 
+
+		});
+
+		wPM.wPlayer.on( "close" , function(code) {
+			console.log( "Exit Code = " + code.toString() );
+		});
+
+	},
+
+	sendCMD: function( wCMD , wARGS ) {
+		wPM.wPlayer.stdin.write([wCMD].concat(wARGS).join(' ') + '\n');
+	},
+
+	pause: function() {
+		wPM.sendCMD( "pause" );
+		wPM.state.paused = !wPM.state.paused;
+		wEmitter.emit("mPlayerPaused");
+	},
+
+	stop: function() {
+		wPM.sendCMD( "stop" );
+		wPM.state.playing = false;
+		wPM.wPlayer.kill();
+		wPM.wPlayer = null;
+		wEmitter.emit("mPlayerStopped");
+	},
+
+	seek: function(seconds) {
+		wPM.sendCMD( "seek" , [ seconds , 2 ] );
+    },
 
 };
 
@@ -360,10 +442,14 @@ var wPM = {
 
 
 mediaFiles.init();
-//wPM.prepare( true , "TV Shows" );
-//wPM.prepare( true , "Movies" );
-wPM.prepare( true , "Music" );
+//wPM.playRandom( "TV Shows" );
+wPM.playNextInTVShow( "Gilmore Girls" );
 
+
+wEmitter.on( "mPlayerPlaying" , function(data) {
+	console.log( "now Playing" );
+	console.log( data );
+});
 
 module.exports.playMedia = function( wRandom , wGenre ) {
 	wPM.prepare( wRandom , wGenre );
